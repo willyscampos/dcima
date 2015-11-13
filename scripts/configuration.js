d26121d480228c1d550ef15c26807c37f472c4c8
willyscampos <willyscampos@gmail.com>
2015-11-13 11:48:06 -0300

    x
    x

---
+++ scripts/configuration.js
@@ -0,0 +1,47 @@
+﻿app.controller('ConfigurationController', function ($rootScope, $scope, $http, $location) {
+    // init
+    var lusr_id = $rootScope.usr_id;
+
+    $scope.record = {
+        usr_id: lusr_id,
+        con_goal: 0,
+        con_month: $rootScope.mes,
+        con_year: $rootScope.ano
+    };
+
+    if (lusr_id == 0) {
+        alert('Usuário ainda não foi identificado');
+        $location.path('/entrar');
+    }
+    
+    $http.post($rootScope.Servidor + '/getgoal', $scope.record).success(goalCallback);
+
+    // behavior
+    $scope.Registro = function () {
+        console.log($scope.record);
+        $http.post($rootScope.Servidor + '/goal', $scope.record).success(confirmaCallback);
+    }
+
+    function confirmaCallback(data, status) {
+        $scope.Resultado = status;
+        if (data.erro == "1") {
+            alert('Verifique sua conexão com a Internet');
+        }
+        else {
+            $location.path('/home');
+        }
+
+    }
+
+    function goalCallback(data, status) {
+        $scope.Resultado = status;
+        if (data.erro == "1") {
+            alert('Verifique sua conexão com a Internet');
+        }
+        else {
+            $scope.record.con_goal = data.con_goal;
+        }
+
+    }
+
+});
---
+++ scripts/login.js
@@ -0,0 +1,32 @@
+﻿app.controller('LoginController', function ($rootScope, $scope, $location, $http) {
+
+    // init
+    $scope.record = {
+        usr_email: '',
+        usr_senha: ''        
+    };
+
+    // behavior
+    $scope.Registro = function () {
+        $http.post($rootScope.Servidor + '/getuser', $scope.record).success(confirmaCallback);
+    }
+
+    function confirmaCallback(data, status) {
+        $scope.Resultado = status;
+        if (data.erro == "1") {
+            alert('Verifique suas informações');
+        }
+        else {
+            if (data.usr_id == "0") {
+                alert('Usuário Inválido!');
+            }
+            else {
+                $rootScope.usr_id = data.usr_id;             
+                $rootScope.usr_name = data.usr_name;                
+                $location.path('/home');
+            }
+        }
+
+    }
+
+});
